5a0e6e121c54009e7859803be7e3c869cd9065a3
starship9 <nishanksaini10@gmail.com>
2017-05-21 20:49:09 +0530

    Added basic comments

--- wineQuality.R
+++ wineQuality.R
@@ -1,10 +1,14 @@
+#Source: "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv"
 library(readr)
 winequality_white <- read.csv("C:/Users/Nishank/Desktop/SNU/RStuff/winequality-white.csv",sep=";")
 head(winequality_white)
 names(winequality_white)
 dim(winequality_white)
 
+#plotting quality distribution
 barplot(table(winequality_white$quality))
+
+#classifying taste based on the quality
 winequality_white$taste<-ifelse(winequality_white$quality<6,'bad','good')
 winequality_white$taste[winequality_white$quality==6]<-'normal'
 winequality_white$taste<-as.factor(winequality_white$taste)
@@ -12,11 +16,15 @@ winequality_white$taste<-as.factor(winequality_white$taste)
 table(winequality_white$taste)
 
 set.seed(123)
+
+#70/30 split for the training and testing data sets
 sampleDataSet<-sample(nrow(winequality_white),nrow(winequality_white)*0.7)
 trainDataSet<-winequality_white[sampleDataSet,]
 test<-winequality_white[-sampleDataSet,]
 
 library(randomForest)
+
+#creating the model using the taste and quality attributes
 model<-randomForest(taste~.-quality,data = trainDataSet)
 model
 
