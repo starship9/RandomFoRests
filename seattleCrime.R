eea452f03179a56c7bdd77ac4cb36a56205d7fd0
starship9 <nishanksaini10@gmail.com>
2017-05-26 00:22:47 +0530

    Forest not being created, need to check attributes

--- seattleCrime.R
+++ seattleCrime.R
@@ -62,5 +62,51 @@ groupDF <- filter(groupDF, n > 31000)
 dim(groupDF)
 groupDF
 ggplot(groupDF, mapping = aes(x = Var1, y = n)) + geom_col()
-
+barplot(table(
+  Seattle_Police_Department_911_Incident_Response$`District/Sector`
+))
+table(Seattle_Police_Department_911_Incident_Response$`District/Sector`)
+table(Seattle_Police_Department_911_Incident_Response$`Hundred Block Location`)
+mean(table(
+  Seattle_Police_Department_911_Incident_Response$`Hundred Block Location`
+))
+Seattle_Police_Department_911_Incident_Response$`Event Clearance Code` <-
+  as.numeric(Seattle_Police_Department_911_Incident_Response$`Event Clearance Code`)
+Seattle_Police_Department_911_Incident_Response <-
+  select(Seattle_Police_Department_911_Incident_Response,
+         `CAD CDW ID`:`Incident Location`)
+codeCluster <-
+  kmeans(Seattle_Police_Department_911_Incident_Response$`Event Clearance Code`,
+         3,
+         nstart = 20)
+codeCluster <-
+  kmeans(Seattle_Police_Department_911_Incident_Response$`Event Clearance Code`,
+         3,
+         nstart = 20)
+plot(
+  Seattle_Police_Department_911_Incident_Response$`Event Clearance Code`,
+  col = codeCluster$cluster
+)
+(tbl_df(
+  table(
+    Seattle_Police_Department_911_Incident_Response$`Event Clearance Code`
+  )
+))
+set.seed(100)
+sampleCrimeSet <-
+  sample(
+    nrow(Seattle_Police_Department_911_Incident_Response),
+    nrow(Seattle_Police_Department_911_Incident_Response) * 0.7
+  )
+trainCrimeSet <-
+  Seattle_Police_Department_911_Incident_Response[sampleCrimeSet, ]
+testCrimeSet <-
+  Seattle_Police_Department_911_Incident_Response[-sampleCrimeSet, ]
+library(randomForest)
+crimeModel <-
+  randomForest( ~ `Event Clearance Group`, data = trainCrimeSet)
+crimeModel <-
+  randomForest(`Event Clearance Group` ~ ., data = trainCrimeSet)
+crimeModel <-
+  randomForest(`Event Clearance Group` ~ . - `Event Clearance Code`, data = trainCrimeSet)
 
