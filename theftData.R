148b6ba3d26deefd575f3052e1927a7578c3be73
starship9 <nishanksaini10@gmail.com>
2017-05-31 16:57:23 +0530

    Minor changes, WIP

--- theftData.R
+++ theftData.R
@@ -6,11 +6,14 @@ names(Theft_Data)
 library(tidyverse)
 Theft_Data <- select(Theft_Data, (1:6))
 names(Theft_Data)
+names(Theft_Data) <-
+  c("name", "theft", "luggage", "pickpocketing", "total", "year")
+
 table(Theft_Data$Name)
 ggplot(Theft_Data, mapping = aes(x = Name, y = Total)) + geom_col() + facet_wrap(~
                                                                                    Year)
 
-theftModel <- lm(Total ~ Year + Name, data = Theft_Data)
+theftModel <- lm(total ~ year + name, data = Theft_Data)
 summary(theftModel)
 plot(theftModel$residuals)
 qqnorm(theftModel$residuals)
@@ -18,17 +21,8 @@ qqline(theftModel$residuals)
 hist(theftModel$residuals)
 
 library(randomForest)
-Theft_Data$Name <- as.factor(Theft_Data$Name)
 theftForest <-
-  randomForest(Name ~ Theft + Year + Total + `Luggage Lifting` + `Pick Pocketing`,
-               data = Theft_Data)
-names(Theft_Data)
-Theft_Data$`Luggage Lifting`
-names(Theft_Data)
-names(Theft_Data) <-
-  c("name", "theft", "luggage", "pickpocketing", "total", "year")
-theftForest <-
-  randomForest(name ~ theft + year + total + luggage + pickpocketing, data = Theft_Data)
+  randomForest(as.factor(name) ~ theft + year + total + luggage + pickpocketing, data = Theft_Data)
 class(Theft_Data$name)
 plot(theftForest)
 summary(theftForest)
