6d5857e8f76663dc24d5612adc7b5053c40a5126
starship9 <nishanksaini10@gmail.com>
2017-06-01 18:33:31 +0530

    WIP

--- theftData.R
+++ theftData.R
@@ -11,42 +11,55 @@ names(Theft_Data) <-
 
 table(Theft_Data$name)
 g <-
-  ggplot(Theft_Data, mapping = aes(x = name, y = total, fill = name)) + geom_col() + facet_wrap( ~
-                                                                                                   year)
+  ggplot(Theft_Data, mapping = aes(x = name, y = total, fill = name)) + geom_col() + facet_wrap(~
+                                                                                                  year)
 
 #Added this since the basic ggplot graph is a bit too congested to make stuff out
 plotly::ggplotly(g)
 
 theftModel <-
-  lm(total ~ year + name + theft + luggage + pickpocketing, data = Theft_Data)
+  lm(total ~ year + name + theft + luggage + pickpocketing, data = theftTrain)
 summary(theftModel)
 plot(theftModel$residuals)
 qqnorm(theftModel$residuals)
 qqline(theftModel$residuals)
 hist(theftModel$residuals)
-predict(theftModel)
+lmPred <- predict(theftModel, newdata = theftTest)
 plot(predict(theftModel))
+
+table(lmPred, theftTest$total)
 
 library(randomForest)
 theftForest <-
   randomForest(as.factor(name) ~ theft + year + total + luggage + pickpocketing,
                data = theftTrain)
 class(Theft_Data$name)
+theftForest
 plot(theftForest)
 summary(theftForest)
 table(predict(theftForest))
 barplot(table(predict(theftForest)))
 
-forestDF<-tbl_df(table(predict(theftForest)))
+newPred <- predict(theftForest, newdata = theftTest)
+table(newPred, theftTest$name)
+
+
+forestDF <- tbl_df(table(predict(theftForest)))
 names(forestDF)
 plotly::ggplotly(ggplot(forestDF, mapping = aes(x = Var1, y = n)) + geom_col())
 
-Theft_Data<-Theft_Data[complete.cases(Theft_Data),]
-Theft_Data$name<-as.factor(Theft_Data$name)
-theftTrain <- Theft_Data[1:499,]
-theftTest <- Theft_Data[500:712,]
-theftTrainNames <- Theft_Data[1:499,1]
-theftTestNames <- Theft_Data[500:712,1]
+Theft_Data <- Theft_Data[complete.cases(Theft_Data), ]
+Theft_Data$name <- as.factor(Theft_Data$name)
+theftTrain <- Theft_Data[1:499, ]
+theftTest <- Theft_Data[500:712, ]
+theftTrainNames <- Theft_Data[1:499, 1]
+theftTestNames <- Theft_Data[500:712, 1]
 
 library(class)
-knnPredictions<-knn(train = theftTrain, cl = theftTrainNames, k = 27,test = theftTest)
+knnPredictions <-
+  knn(
+    train = theftTrain,
+    cl = theftTrainNames,
+    k = 27,
+    test = theftTest
+  )
